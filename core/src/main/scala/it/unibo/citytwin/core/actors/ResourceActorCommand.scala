d95387b8ce66313247e8b84dc9cc2bd8b4aba133
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-02 12:41:58 +0200

    feat: add resourceService

--- core/src/main/scala/it/unibo/citytwin/core/actors/ResourceActorCommand.scala
+++ core/src/main/scala/it/unibo/citytwin/core/actors/ResourceActorCommand.scala
@@ -1,7 +1,12 @@
 package it.unibo.citytwin.core.actors
 
+import akka.actor.typed.receptionist.ServiceKey
 import it.unibo.citytwin.core.model.Resource
 
 trait ResourceActorCommand
 case class ResponseResourceState(resource: Option[Resource])
     extends ResourceActorCommand
+
+object ResourceActor:
+  val resourceService: ServiceKey[ResourceActorCommand] =
+    ServiceKey[ResourceActorCommand]("resourceService")
