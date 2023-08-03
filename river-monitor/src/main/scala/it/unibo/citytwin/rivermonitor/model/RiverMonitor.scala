0322cd3dda7c03b7d81d6405e62171388a7ce7b3
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-03 14:54:08 +0200

    refactor: remove zone from RiverMonitor; RiverMonitorState is 'Safe' insthead of 'Free'

--- river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/model/RiverMonitor.scala
+++ river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/model/RiverMonitor.scala
@@ -4,11 +4,10 @@ import it.unibo.citytwin.core.model.Point2D
 import it.unibo.citytwin.rivermonitor.model.RiverMonitorState.RiverMonitorState
 object RiverMonitorState extends Enumeration:
   type RiverMonitorState = Value
-  val Free, Evacuating, Warned = Value
+  val Safe, Evacuating, Warned = Value
 
-case class RiverMonitor(zoneId: Int,
-                       position: Point2D[Int],
+case class RiverMonitor(position: Point2D[Int],
                        state: RiverMonitorState):
 
   def state_(newState: RiverMonitorState): RiverMonitor =
-    RiverMonitor(zoneId, position, newState)
+    RiverMonitor(position, newState)
