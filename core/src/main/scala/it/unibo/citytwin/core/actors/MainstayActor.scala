ed65c097857804bb48cb85437e74abf01e1027e8
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-03 15:53:20 +0200

    fix: mainstay actor was sending wrong resource updates

--- core/src/main/scala/it/unibo/citytwin/core/actors/MainstayActor.scala
+++ core/src/main/scala/it/unibo/citytwin/core/actors/MainstayActor.scala
@@ -47,7 +47,7 @@ object MainstayActor:
         }
         case UpdateResources(update: Map[ActorRef[ResourceActorCommand], Resource]) => {
           ctx.log.debug("UpdateResources")
-          mainstays.foreach((k, _) => k ! UpdateResources(resources))
+          mainstays.foreach((k, v) => if v then k ! UpdateResources(update))
           val result: Map[ActorRef[ResourceActorCommand], Resource] = resources.map((k, v) =>
             if update.contains(k) then (k, v.merge(update(k))) else (k, v)
           ) ++ (update -- resources.keys)
