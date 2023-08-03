2041e6c8176a49741604743deb613c69d9052947
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-03 15:07:20 +0200

    feat: simplify Mainstay APIs

--- core/src/main/scala/it/unibo/citytwin/core/actors/MainstayActor.scala
+++ core/src/main/scala/it/unibo/citytwin/core/actors/MainstayActor.scala
@@ -6,7 +6,7 @@ import akka.actor.typed.{ActorRef, Behavior}
 import it.unibo.citytwin.core.Serializable
 import it.unibo.citytwin.core.model.Resource
 
-import scala.collection.immutable.Set
+import scala.collection.immutable.{Map, Set}
 
 trait MainstayActorCommand
 case class AskResourcesState(
@@ -14,16 +14,11 @@ case class AskResourcesState(
     names: Set[String]
 ) extends MainstayActorCommand
     with Serializable
-case class SetResourceState(
-    ref: ActorRef[ResourceActorCommand],
-    resource: Resource
+case class UpdateResources(
+                            update: Map[ActorRef[ResourceActorCommand], Resource]
 ) extends MainstayActorCommand
     with Serializable
-case class SetMainstayActors(nodes: Map[ActorRef[MainstayActorCommand], Boolean])
-    extends MainstayActorCommand
-    with Serializable
-
-case class SetResourceNodesState(nodes: Map[ActorRef[ResourceActorCommand], Boolean])
+case class SetMainstays(nodes: Map[ActorRef[MainstayActorCommand], Boolean])
     extends MainstayActorCommand
     with Serializable
 
@@ -43,28 +38,18 @@ object MainstayActor:
             ) => {
           ctx.log.debug("AskResourceState")
           replyTo ! ResponseResourceState(
-            resources.values.filter(x => names.contains(x.name)).toSet
+            resources.values.filter(x => x.name.isDefined).filter(x => names.contains(x.name.get)).toSet
           )
           Behaviors.same
         }
-        case SetResourceState(
-              ref: ActorRef[ResourceActorCommand],
-              resource: Resource
-            ) => {
-          ctx.log.debug("SetResourceState")
-          mainstays.foreach((k, _) => k ! SetResourceState(ref, resource))
-          MainstayActor(mainstays, resources + (ref -> resource))
-        }
-        case SetResourceNodesState(nodes: Map[ActorRef[ResourceActorCommand], Boolean]) => {
-          ctx.log.debug("SetResourceNodesState")
-          val result: Map[ActorRef[ResourceActorCommand], Resource] = for
-            (k, v)   <- resources
-            (k2, v2) <- nodes
-          yield if k == k2 then (k, v.copy(nodeState = v2)) else (k2, Resource(nodeState = v2))
+        case UpdateResources(update: Map[ActorRef[ResourceActorCommand], Resource]) => {
+          ctx.log.debug("UpdateResources")
+          mainstays.foreach((k, _) => k ! UpdateResources(resources))
+          val result: Map[ActorRef[ResourceActorCommand], Resource] = resources.map((k, v) => if update.contains(k) then (k, v.merge(update(k))) else (k, v)) ++ (update -- resources.keys)
           MainstayActor(mainstays, result)
         }
-        case SetMainstayActors(nodes: Map[ActorRef[MainstayActorCommand], Boolean]) => {
-          ctx.log.debug("SetMainstayActors")
+        case SetMainstays(nodes: Map[ActorRef[MainstayActorCommand], Boolean]) => {
+          ctx.log.debug("SetMainstays")
           MainstayActor(nodes, resources)
         }
       }
--- core/src/main/scala/it/unibo/citytwin/core/actors/NodesObserverActor.scala
+++ core/src/main/scala/it/unibo/citytwin/core/actors/NodesObserverActor.scala
@@ -2,6 +2,7 @@ package it.unibo.citytwin.core.actors
 
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
+import it.unibo.citytwin.core.model.Resource
 
 trait NodesObserverActorCommand
 case class UpdateMainstayNodesState(refs: Set[ActorRef[MainstayActorCommand]])
@@ -23,14 +24,14 @@ object NodesObserverActor:
           ctx.log.debug("UpdateMainstaysState")
           val result: Map[ActorRef[MainstayActorCommand], Boolean] =
             mainstays.map((k, _) => (k, false)) ++ refs.map(k => (k, true))
-          mainstay ! SetMainstayActors(result)
+          mainstay ! SetMainstays(result)
           NodesObserverActor(mainstay, result, resources)
         }
         case UpdateResourceNodesState(refs: Set[ActorRef[ResourceActorCommand]]) => {
           ctx.log.debug("UpdateResourcesState")
           val result: Map[ActorRef[ResourceActorCommand], Boolean] =
             resources.map((k, _) => (k, false)) ++ refs.map(k => (k, true))
-          mainstay ! SetResourceNodesState(result)
+          mainstay ! UpdateResources(result.map((k, v) => (k, Resource(nodeState = Some(v)))))
           NodesObserverActor(mainstay, mainstays, result)
         }
       }
