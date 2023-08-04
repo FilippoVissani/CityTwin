c3d1d307865af0fa23308933220d1daa5ffb8188
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-04 13:23:37 +0200

    chore: add object as Main

--- core/src/main/scala/it/unibo/citytwin/core/Main.scala
+++ core/src/main/scala/it/unibo/citytwin/core/Main.scala
@@ -1,11 +1,11 @@
 package it.unibo.citytwin.core
 
-import akka.actor.typed.ActorSystem
+import akka.actor.typed.{ActorSystem, Behavior}
 import com.typesafe.config.{Config, ConfigFactory}
 import it.unibo.citytwin.core.actors.MainstayActor
 
-@main def main(port: String = "2551"): Unit =
-  val config: Config = ConfigFactory
-    .parseString(s"""akka.remote.artery.canonical.port=$port""")
+object Main extends App:
+  private val config: Config = ConfigFactory
+    .parseString(s"""akka.remote.artery.canonical.port=2551""")
     .withFallback(ConfigFactory.load("cluster"))
   ActorSystem(MainstayActor(), "ClusterSystem", config)
