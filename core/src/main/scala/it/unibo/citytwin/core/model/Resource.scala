4d50e0b91a3e7d604d03e993113df16dc1fcf6d4
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-08 16:04:59 +0200

    feat: simplify Resource, need to be serialized

--- core/src/main/scala/it/unibo/citytwin/core/model/Resource.scala
+++ core/src/main/scala/it/unibo/citytwin/core/model/Resource.scala
@@ -1,38 +1,23 @@
 package it.unibo.citytwin.core.model
 
-enum ResourceType:
-  case Act
-  case Sense
+import it.unibo.citytwin.core.model.ResourceType.ResourceType
 
-trait Resource:
-  def name: Option[String]
-  def position: Option[Point2D[Int]]
-  def state: Option[Any]
-  def resourceType: Set[ResourceType]
-  def nodeState: Option[Boolean]
-  def merge(other: Resource): Resource
+object ResourceType extends Enumeration:
+  type ResourceType = Value
+  val Act, Sense = Value
 
-object Resource:
-  def apply(
-      name: Option[String] = None,
-      position: Option[Point2D[Int]] = None,
-      state: Option[Any] = None,
-      resourceType: Set[ResourceType] = Set(),
-      nodeState: Option[Boolean] = None
-  ): Resource = ResourceImpl(name, position, state, resourceType, nodeState)
-
-  private case class ResourceImpl(
-      override val name: Option[String],
-      override val position: Option[Point2D[Int]],
-      override val state: Option[Any],
-      override val resourceType: Set[ResourceType],
-      override val nodeState: Option[Boolean]
-  ) extends Resource:
-    override def merge(other: Resource): Resource =
-      val name     = if other.name.isDefined then other.name else this.name
-      val position = if other.position.isDefined then other.position else this.position
-      val state    = if other.state.isDefined then other.state else this.state
-      val resourceType =
-        if other.resourceType.nonEmpty then other.resourceType else this.resourceType
-      val nodeState = if other.nodeState.isDefined then other.nodeState else this.nodeState
-      Resource(name, position, state, resourceType, nodeState)
+case class Resource(
+                     name: Option[String] = None,
+                     position: Option[Point2D[Int]] = None,
+                     state: Option[Any] = None,
+                     resourceType: Set[ResourceType] = Set(),
+                     nodeState: Option[Boolean] = None
+                   ):
+  def merge(other: Resource): Resource =
+    val name = if other.name.isDefined then other.name else this.name
+    val position = if other.position.isDefined then other.position else this.position
+    val state = if other.state.isDefined then other.state else this.state
+    val resourceType =
+      if other.resourceType.nonEmpty then other.resourceType else this.resourceType
+    val nodeState = if other.nodeState.isDefined then other.nodeState else this.nodeState
+    Resource(name, position, state, resourceType, nodeState)
