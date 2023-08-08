31f13da39bde9025e1ac99fee36b39ff06e577f3
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-08 12:38:09 +0200

    test: add test for controlPanel

---
+++ control-panel/src/test/scala/it/unibo/citytwin/control_panel/actors/AsyncTestingControlPanelSpec.scala
@@ -0,0 +1,12 @@
+package it.unibo.citytwin.control_panel.actors
+
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class AsyncTestingControlPanelSpec extends AnyWordSpec with BeforeAndAfterAll with Matchers:
+
+  val testKit: ActorTestKit = ActorTestKit()
+
+  override def afterAll(): Unit = testKit.shutdownTestKit()
