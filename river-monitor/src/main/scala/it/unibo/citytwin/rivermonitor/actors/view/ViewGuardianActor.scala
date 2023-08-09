4c77d2515338584a0d5cb73e6541423fd92b5234
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-09 13:38:28 +0200

    feat: ViewGuardianActor

---
+++ river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/actors/view/ViewGuardianActor.scala
@@ -0,0 +1,33 @@
+package it.unibo.citytwin.rivermonitor.actors.view
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.receptionist.Receptionist
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import it.unibo.citytwin.core.actors.MainstayActor.mainstayService
+import it.unibo.citytwin.core.actors.SetMainstayActorsToResourceActor
+
+object ViewGuardianActor:
+  def apply(viewName: String,
+            width: Int,
+            height: Int): Behavior[Nothing] = 
+    Behaviors.setup[Receptionist.Listing] { ctx =>
+      val viewActor = ctx.spawnAnonymous(Behaviors.setup(new ViewActor(_, viewName, width, height)))
+      val resourceViewActor = ctx.spawnAnonymous(ResourceViewActor(viewActor))
+      viewActor ! SetResourceActor(resourceViewActor)
+      
+      ctx.system.receptionist ! Receptionist.subscribe(mainstayService, ctx.self)
+
+      Behaviors.receiveMessagePartial[Receptionist.Listing] {
+        case mainstayService.Listing(listings) =>{
+          ctx.log.debug("Received mainstayService")
+          //tell all mainstayActors to resourceViewActor
+          resourceViewActor ! SetMainstayActorsToResourceActor(listings)
+          Behaviors.same
+        }
+        case _ => {
+          ctx.log.debug("Received Stop")
+          Behaviors.stopped
+        }
+      }
+    }.narrow
+
