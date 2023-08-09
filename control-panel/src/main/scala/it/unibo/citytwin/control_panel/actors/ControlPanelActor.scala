9763578391c40aa7c8f7e3d77ba0202af8d982a9
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-09 15:00:45 +0200

    feat: now ControlPanelActor spawns ResourceActor

--- control-panel/src/main/scala/it/unibo/citytwin/control_panel/actors/ControlPanelActor.scala
+++ control-panel/src/main/scala/it/unibo/citytwin/control_panel/actors/ControlPanelActor.scala
@@ -3,7 +3,7 @@ package it.unibo.citytwin.control_panel.actors
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.util.Timeout
-import it.unibo.citytwin.core.actors.{AskAllResourcesToMainstay, AskResourcesToMainstay, ResourceActorCommand, ResourcesFromMainstayResponse}
+import it.unibo.citytwin.core.actors.{AskAllResourcesToMainstay, AskResourcesToMainstay, ResourceActor, ResourceActorCommand, ResourcesFromMainstayResponse}
 import it.unibo.citytwin.core.model.Resource
 
 import concurrent.duration.DurationInt
@@ -13,15 +13,17 @@ trait ControlPanelActorCommand
 case class AdaptedResourcesFromMainstayResponse(resources: Set[Resource]) extends ControlPanelActorCommand with Serializable
 object Tick extends ControlPanelActorCommand with Serializable
 object ControlPanelActor:
-  def apply(resourceActor: ActorRef[ResourceActorCommand]): Behavior[ControlPanelActorCommand] =
+  def apply(resourceActor: Option[ActorRef[ResourceActorCommand]] = None): Behavior[ControlPanelActorCommand] =
     Behaviors.setup[ControlPanelActorCommand] { ctx =>
       implicit val timeout: Timeout = 3.seconds
+      if resourceActor.isEmpty then
+        ControlPanelActor(Some(ctx.spawnAnonymous(ResourceActor())))
       Behaviors.withTimers { timers =>
         timers.startTimerAtFixedRate(Tick, 5.seconds)
         Behaviors.receiveMessage {
           case AdaptedResourcesFromMainstayResponse(resources: Set[Resource]) => ???
           case Tick => {
-            ctx.ask(resourceActor, ref => AskAllResourcesToMainstay(ref)) {
+            ctx.ask(resourceActor.get, ref => AskAllResourcesToMainstay(ref)) {
               case Success(ResourcesFromMainstayResponse(resources: Set[Resource])) => AdaptedResourcesFromMainstayResponse(resources)
               case _ => AdaptedResourcesFromMainstayResponse(Set())
             }
