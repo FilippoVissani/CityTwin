0a18f0f3e7ac089dc7675a2d1edc5c4d778fdfa9
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-09 11:20:21 +0200

    feat: now ResourceGuardianActor takes ResourceActorCommand as parameter

--- core/src/main/scala/it/unibo/citytwin/core/actors/ResourceGuardianActor.scala
+++ core/src/main/scala/it/unibo/citytwin/core/actors/ResourceGuardianActor.scala
@@ -1,15 +1,14 @@
 package it.unibo.citytwin.core.actors
 
-import akka.actor.typed.Behavior
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.receptionist.Receptionist
 import akka.actor.typed.scaladsl.Behaviors
 import it.unibo.citytwin.core.actors.MainstayActor.mainstayService
 
 object ResourceGuardianActor:
-  def apply(): Behavior[Nothing] =
+  def apply(resourceActor: ActorRef[ResourceActorCommand]): Behavior[Nothing] =
     Behaviors
       .setup[Receptionist.Listing] { ctx =>
-        val resourceActor = ctx.spawnAnonymous(ResourceActor())
         ctx.system.receptionist ! Receptionist.Subscribe(
           mainstayService,
           ctx.self
