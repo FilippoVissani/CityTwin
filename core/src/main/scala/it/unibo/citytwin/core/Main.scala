acb3da996649a7005bc3b572db1a695cea40b162
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-09 12:27:26 +0200

    feat: use ActorSystemStarter in Main

--- core/src/main/scala/it/unibo/citytwin/core/Main.scala
+++ core/src/main/scala/it/unibo/citytwin/core/Main.scala
@@ -2,10 +2,9 @@ package it.unibo.citytwin.core
 
 import akka.actor.typed.{ActorSystem, Behavior}
 import com.typesafe.config.{Config, ConfigFactory}
+import it.unibo.citytwin.core.ActorSystemStarter.startup
 import it.unibo.citytwin.core.actors.MainstayActor
 
 object Main extends App:
-  private val config: Config = ConfigFactory
-    .parseString(s"""akka.remote.artery.canonical.port=2551""")
-    .withFallback(ConfigFactory.load("cluster"))
-  ActorSystem(MainstayActor(), "ClusterSystem", config)
+  private val port: Int = if args.length == 0 then 2551 else args(0).toInt
+  startup(port = port)(MainstayActor())
