01ec943a337d53ded27f8606e1846f2b5537af36
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-10 18:44:37 +0200

    fix: correct test for FloodSensor should send measurement to Mainstay

--- river-monitor/src/test/scala/it/unibo/citytwin/rivermonitor/AsyncTestingFloodSensorSpec.scala
+++ river-monitor/src/test/scala/it/unibo/citytwin/rivermonitor/AsyncTestingFloodSensorSpec.scala
@@ -5,10 +5,10 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import akka.actor.testkit.typed.scaladsl.ActorTestKit
 import akka.actor.typed.receptionist.Receptionist
-import it.unibo.citytwin.core.actors.{MainstayActor, MainstayActorCommand, SetMainstayActorsToResourceActor, SetResourceState}
+import it.unibo.citytwin.core.actors.{MainstayActor, MainstayActorCommand, SetMainstayActorsToResourceActor, UpdateResources}
 import it.unibo.citytwin.core.actors.ResourceActor.resourceService
 import it.unibo.citytwin.core.model.{Point2D, Resource, ResourceType}
-import it.unibo.citytwin.rivermonitor.actors.floodsensor.{FloodSensorActor, FloodSensorGuardianActor, ResourceFloodSensorActor}
+import it.unibo.citytwin.rivermonitor.actors.floodsensor.{FloodSensorActor, FloodSensorGuardianActor, ResourceFloodSensorActor, SetResourceActor}
 import it.unibo.citytwin.rivermonitor.model.FloodSensor
 
 import scala.concurrent.duration.DurationInt
@@ -20,22 +20,16 @@ class AsyncTestingFloodSensorSpec extends AnyWordSpec with BeforeAndAfterAll wit
 
   "FloodSensor" should {
     "send measurement to Mainstay" in {
-      //val mainstayActor = testKit.spawn(MainstayActor())
       val mainstayProbe = testKit.createTestProbe[MainstayActorCommand]()
 
-      //val floodSensorGuardianActor = testKit.spawn(FloodSensorGuardianActor(dummyFloodSensor))
       val dummyFloodSensor = FloodSensor("sensor1", Point2D[Int](0, 0))
-      val floodSensorActor = testKit.spawn(FloodSensorActor(dummyFloodSensor)) //used to start resourceFloodSensorActor
-
+      val floodSensorActor = testKit.spawn(FloodSensorActor(dummyFloodSensor))
       val resourceFloodSensorActor = testKit.spawn(ResourceFloodSensorActor(floodSensorActor))
-      val dummyResource = Resource("sensor1", Some(Point2D[Int](0, 0)), Some(2), Set(ResourceType.Sense))
-
+      floodSensorActor ! SetResourceActor(resourceFloodSensorActor)
       resourceFloodSensorActor ! SetMainstayActorsToResourceActor(Set(mainstayProbe.ref))
-      //mainstayProbe ! SetResourceState(resourceFloodSensorActor, Some(dummyResource))
-      mainstayProbe.within(10.seconds){
-        mainstayProbe.expectMessageType[SetResourceState]
-      }
 
-      //testKit.stop(floodSensorGuardianActor)
+      mainstayProbe.within(10.seconds){
+        mainstayProbe.expectMessageType[UpdateResources]
+      }
     }
   }
