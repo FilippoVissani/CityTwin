2e9d76fee8482fce85f0fd914aacfd743a0e400f
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-10 11:50:27 +0200

    chore: remove unused import

--- control-panel/src/main/scala/it/unibo/citytwin/control_panel/Main.scala
+++ control-panel/src/main/scala/it/unibo/citytwin/control_panel/Main.scala
@@ -3,7 +3,6 @@ package it.unibo.citytwin.control_panel
 import akka.actor.typed.ActorSystem
 import it.unibo.citytwin.control_panel.actors.{ControlPanelActor, ControlPanelActorCommand}
 import it.unibo.citytwin.core.ActorSystemStarter.startup
-import it.unibo.citytwin.core.Main.args
 
 @main def main(args: String*): ActorSystem[ControlPanelActorCommand] =
     val port: Int = if args.isEmpty then 2551 else args(0).toInt
