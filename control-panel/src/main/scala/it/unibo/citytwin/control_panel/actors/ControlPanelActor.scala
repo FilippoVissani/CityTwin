f7fd778d571fa15c4e067f664a4de394fbb58d40
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-10 11:48:53 +0200

    feat: now ControlPanelActor asks mainstays state

--- control-panel/src/main/scala/it/unibo/citytwin/control_panel/actors/ControlPanelActor.scala
+++ control-panel/src/main/scala/it/unibo/citytwin/control_panel/actors/ControlPanelActor.scala
@@ -4,7 +4,7 @@ import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.util.Timeout
 import it.unibo.citytwin.control_panel.view.View
-import it.unibo.citytwin.core.actors.{AskAllResourcesToMainstay, AskResourcesToMainstay, ResourceActor, ResourceActorCommand, ResourcesFromMainstayResponse}
+import it.unibo.citytwin.core.actors.{AskAllResourcesToMainstay, AskMainstaysState, AskResourcesToMainstay, MainstayActorCommand, MainstaysStateResponse, ResourceActor, ResourceActorCommand, ResourcesFromMainstayResponse}
 import it.unibo.citytwin.core.model.Resource
 
 import concurrent.duration.DurationInt
@@ -12,21 +12,31 @@ import scala.util.Success
 
 trait ControlPanelActorCommand
 case class AdaptedResourcesFromMainstayResponse(resources: Set[Resource]) extends ControlPanelActorCommand with Serializable
+
+case class AdaptedMainstaysStateResponse(mainstays: Set[ActorRef[MainstayActorCommand]]) extends ControlPanelActorCommand with Serializable
 object Tick extends ControlPanelActorCommand with Serializable
 object ControlPanelActor:
   def apply(): Behavior[ControlPanelActorCommand] =
     Behaviors.setup[ControlPanelActorCommand] { ctx =>
       implicit val timeout: Timeout = 3.seconds
       val resourceActor = ctx.spawnAnonymous(ResourceActor())
-      val gui = View()
+      val view = View()
       Behaviors.withTimers { timers =>
         timers.startTimerAtFixedRate(Tick, 5.seconds)
         Behaviors.receiveMessage {
           case AdaptedResourcesFromMainstayResponse(resources: Set[Resource]) => {
-            gui.drawResources(resources)
+            view.drawResources(resources)
+            Behaviors.same
+          }
+          case AdaptedMainstaysStateResponse(mainstays: Set[ActorRef[MainstayActorCommand]]) => {
+            view.drawMainstays(mainstays)
             Behaviors.same
           }
           case Tick => {
+            ctx.ask(resourceActor, ref => AskMainstaysState(ref)) {
+              case Success(MainstaysStateResponse(mainstays: Set[ActorRef[MainstayActorCommand]])) => AdaptedMainstaysStateResponse(mainstays)
+              case _ => AdaptedMainstaysStateResponse(Set())
+            }
             ctx.ask(resourceActor, ref => AskAllResourcesToMainstay(ref)) {
               case Success(ResourcesFromMainstayResponse(resources: Set[Resource])) => AdaptedResourcesFromMainstayResponse(resources)
               case _ => AdaptedResourcesFromMainstayResponse(Set())
