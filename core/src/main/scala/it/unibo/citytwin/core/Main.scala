66f49c94a6cb0cb1ff443c8aa13dd2f9bf906451
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-10 11:51:11 +0200

    fix: fix Main

--- core/src/main/scala/it/unibo/citytwin/core/Main.scala
+++ core/src/main/scala/it/unibo/citytwin/core/Main.scala
@@ -3,8 +3,8 @@ package it.unibo.citytwin.core
 import akka.actor.typed.{ActorSystem, Behavior}
 import com.typesafe.config.{Config, ConfigFactory}
 import it.unibo.citytwin.core.ActorSystemStarter.startup
-import it.unibo.citytwin.core.actors.MainstayActor
+import it.unibo.citytwin.core.actors.{MainstayActor, MainstayActorCommand}
 
-object Main extends App:
-  private val port: Int = if args.length == 0 then 2551 else args(0).toInt
+@main def main(args: String*): ActorSystem[MainstayActorCommand] =
+  val port: Int = if args.isEmpty then 2551 else args(0).toInt
   startup(port = port)(MainstayActor())
