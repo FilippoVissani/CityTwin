1564561bb14e8cc26cf10068ebc0732c2f38c03b
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-11 13:31:48 +0200

    test: RiverMonitor tests for state change

---
+++ river-monitor/src/test/scala/it/unibo/citytwin/rivermonitor/AsyncTestingRiverMonitorSpec.scala
@@ -0,0 +1,49 @@
+package it.unibo.citytwin.rivermonitor
+
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import it.unibo.citytwin.core.actors.{MainstayActorCommand, SetMainstayActorsToResourceActor, UpdateResources}
+import it.unibo.citytwin.core.model.{Point2D, Resource}
+import it.unibo.citytwin.core.model.ResourceType.{Act, Sense}
+import it.unibo.citytwin.rivermonitor.actors.rivermonitor.{EvacuatedRiverMonitor, EvacuatingRiverMonitor, ResourceRiverMonitorActor, RiverMonitorActor, SetResourceActor, WarnRiverMonitor}
+import it.unibo.citytwin.rivermonitor.model.RiverMonitor
+import it.unibo.citytwin.rivermonitor.model.RiverMonitorState.Safe
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class AsyncTestingRiverMonitorSpec extends AnyWordSpec with BeforeAndAfterAll with Matchers:
+
+  val testKit: ActorTestKit = ActorTestKit()
+  override def afterAll(): Unit = testKit.shutdownTestKit()
+
+  "RiverMonitor" should {
+    val mainstayProbe = testKit.createTestProbe[MainstayActorCommand]()
+
+    val dummyRiverMonitor = RiverMonitor("riverMonitor1", Point2D[Int](0, 0), Safe)
+    //Guardian work
+    val riverMonitorActor = testKit.spawn(RiverMonitorActor(dummyRiverMonitor))
+    val resourceRiverMonitorActor = testKit.spawn(ResourceRiverMonitorActor(riverMonitorActor = riverMonitorActor, resourcesToCheck = Set()))
+    riverMonitorActor ! SetResourceActor(resourceRiverMonitorActor)
+    resourceRiverMonitorActor ! SetMainstayActorsToResourceActor(Set(mainstayProbe.ref))
+    //end Guardian work
+    var dummyResource = Resource(Some(dummyRiverMonitor.riverMonitorName), Some(dummyRiverMonitor.position), Some(dummyRiverMonitor.state.toString), Set(Sense, Act))
+
+    "send its state (Safe) to Mainstay when it's started" in {
+      mainstayProbe.expectMessage(UpdateResources(Map(resourceRiverMonitorActor -> dummyResource).toSet))
+    }
+    "send its state (Warned) to Mainstay when receive WarnRiverMonitor" in {
+      riverMonitorActor ! WarnRiverMonitor
+      dummyResource = dummyResource.copy(state = Some("Warned"))
+      mainstayProbe.expectMessage(UpdateResources(Map(resourceRiverMonitorActor -> dummyResource).toSet))
+    }
+    "send its state (Evacuating) to Mainstay when receive EvacuatingRiverMonitor" in {
+      riverMonitorActor ! EvacuatingRiverMonitor
+      dummyResource = dummyResource.copy(state = Some("Evacuating"))
+      mainstayProbe.expectMessage(UpdateResources(Map(resourceRiverMonitorActor -> dummyResource).toSet))
+    }
+    "send its state (Safe) to Mainstay when receive EvacuatedRiverMonitor" in {
+      riverMonitorActor ! EvacuatedRiverMonitor
+      dummyResource = dummyResource.copy(state = Some("Safe"))
+      mainstayProbe.expectMessage(UpdateResources(Map(resourceRiverMonitorActor -> dummyResource).toSet))
+    }
+  }
