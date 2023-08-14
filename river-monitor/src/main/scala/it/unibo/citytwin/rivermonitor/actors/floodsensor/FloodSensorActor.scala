d1742087850f44338976ecd1ee7a664e00dd5f7f
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-14 12:10:28 +0200

    feat: FloodSensorActor standardized to use ResourceActor

--- river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/actors/floodsensor/FloodSensorActor.scala
+++ river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/actors/floodsensor/FloodSensorActor.scala
@@ -1,20 +1,15 @@
 package it.unibo.citytwin.rivermonitor.actors.floodsensor
 
 import akka.actor
-import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
-import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors, TimerScheduler}
-import akka.actor.typed.{ActorRef, Behavior, SupervisorStrategy}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.util.Timeout
 import it.unibo.citytwin.core.actors.*
-import it.unibo.citytwin.core.actors.ResourceActor.resourceService
 import it.unibo.citytwin.core.model.{Resource, ResourceType}
 import it.unibo.citytwin.core.Serializable
-import it.unibo.citytwin.rivermonitor.actors.floodsensor.{FloodSensorActorCommand, SetResourceActor, Tick}
 import it.unibo.citytwin.rivermonitor.model.FloodSensor
-
 import java.util.concurrent.ThreadLocalRandom
 import scala.concurrent.duration.DurationInt
-import scala.util.{Failure, Success}
 
 trait FloodSensorActorCommand
 
@@ -23,40 +18,26 @@ trait FloodSensorActorCommand
  * This object is used to trigger the FloodSensorActor to perform periodic tasks.
  */
 object Tick extends Serializable with FloodSensorActorCommand
-/**
- * A message received by the FloodSensorActor to set the reference to the ResourceActor.
- *
- * @param resourceActor The reference to the ResourceActor to communicate with.
- */
-case class SetResourceActor(resourceActor: ActorRef[ResourceActorCommand]) extends Serializable with FloodSensorActorCommand
 
 object FloodSensorActor :
   def apply(floodSensor: FloodSensor): Behavior[FloodSensorActorCommand] =
     Behaviors.setup[FloodSensorActorCommand] { ctx =>
+      implicit val timeout: Timeout = 3.seconds
+      val resourceActor = ctx.spawnAnonymous(ResourceActor())
       Behaviors.withTimers { timers =>
         timers.startTimerAtFixedRate(Tick, 5.seconds)
-        FloodSensorActorLogic(ctx, floodSensor, Option.empty)
-      }
-    }
-
-  def FloodSensorActorLogic(ctx: ActorContext[FloodSensorActorCommand],
-                            floodSensor: FloodSensor,
-                            resourceActor: Option[ActorRef[ResourceActorCommand]]): Behavior[FloodSensorActorCommand] =
-    implicit val timeout: Timeout = 2.seconds
-    Behaviors.receiveMessage {
-      case Tick => {
-        ctx.log.debug(s"Received Tick")
-        val waterLevel = ThreadLocalRandom.current().nextFloat(20)
-        val resource = Resource(Some(floodSensor.name), Some(floodSensor.position), Some(waterLevel), Set(ResourceType.Sense))
-        if (resourceActor.nonEmpty) resourceActor.get ! ResourceChanged(resource)
-        Behaviors.same
-      }
-      case SetResourceActor(resourceActor) => {
-        ctx.log.debug(s"Received SetResourceActor")
-        FloodSensorActorLogic(ctx, floodSensor, Some(resourceActor))
-      }
-      case _ => {
-        ctx.log.debug(s"Unexpected message. The actor is being stopped")
-        Behaviors.stopped
+        Behaviors.receiveMessage {
+          case Tick => {
+            ctx.log.debug(s"Received Tick")
+            val waterLevel = ThreadLocalRandom.current().nextFloat(20)
+            val resource = Resource(Some(floodSensor.name), Some(floodSensor.position), Some(waterLevel), Set(ResourceType.Sense))
+            resourceActor ! ResourceChanged(resource)
+            Behaviors.same
+          }
+          case _ => {
+            ctx.log.debug(s"Unexpected message. The actor is being stopped")
+            Behaviors.stopped
+          }
+        }
       }
     }
