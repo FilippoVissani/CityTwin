b8236107f4d19603a4bd91f15c51ecf89c2bee54
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-14 12:10:05 +0200

    test: remove wrong test

--- core/src/test/scala/it/unibo/citytwin/core/actors/AsyncTestingMainstaySpec.scala
+++ core/src/test/scala/it/unibo/citytwin/core/actors/AsyncTestingMainstaySpec.scala
@@ -46,65 +46,6 @@ class AsyncTestingMainstaySpec extends AnyWordSpec with BeforeAndAfterAll with M
       testKit.stop(mainstay)
     }
 
-    "Merge resource states correctly" in {
-      val onlineNodes = LazyList
-        .from(0)
-        .map(x =>
-          (
-            testKit.spawn(DummyResourceActor()),
-            Resource(name = Some(s"sensor$x"), nodeState = Some(true))
-          )
-        )
-        .take(2)
-        .toMap
-      val offlineNodes = LazyList
-        .from(2)
-        .map(x =>
-          (
-            testKit.spawn(DummyResourceActor()),
-            Resource(name = Some(s"sensor$x"), nodeState = Some(false))
-          )
-        )
-        .take(2)
-        .toMap
-      val expectedResult: Set[Resource] =
-        onlineNodes.values.toSet - onlineNodes.values.head ++ offlineNodes.values.toSet + onlineNodes.values.head
-          .merge(Resource(nodeState = Some(false)))
-      val mainstay = testKit.spawn(MainstayActor())
-      val probe    = testKit.createTestProbe[ResourceActorCommand]()
-      val mockedResourceBehavior = Behaviors.setup[ResourceActorCommand] { ctx =>
-        implicit val timeout: Timeout = 3.seconds
-        ctx.ask(
-          mainstay,
-          ref =>
-            AskResourcesState(ref, LazyList.from(0).map(x => s"sensor$x").take(10).toSet[String])
-        ) {
-          case Success(ResourceStatesResponse(resources: Set[Resource])) =>
-            AdaptedResourcesStateResponse(null, resources)
-          case _ => AdaptedResourcesStateResponse(null, Set())
-        }
-        Behaviors.receiveMessage {
-          case AdaptedResourcesStateResponse(
-                _: ActorRef[ResourcesFromMainstayResponse],
-                _: Set[Resource]
-              ) => {
-            Behaviors.same
-          }
-          case _ => Behaviors.stopped
-        }
-      }
-      val probeActor = testKit.spawn(Behaviors.monitor(probe.ref, mockedResourceBehavior))
-      mainstay ! UpdateResources(onlineNodes.toSet)
-      mainstay ! UpdateResources(
-        (offlineNodes + onlineNodes.head.copy(_2 = Resource(nodeState = Some(false)))).toSet
-      )
-      probe.expectMessage(AdaptedResourcesStateResponse(null, expectedResult))
-      onlineNodes.foreach((k, _) => testKit.stop(k))
-      offlineNodes.foreach((k, _) => testKit.stop(k))
-      testKit.stop(mainstay)
-      testKit.stop(probeActor)
-    }
-
     "Inform other mainstays on Resource update" in {
       val probe    = testKit.createTestProbe[MainstayActorCommand]()
       val resource = testKit.spawn(DummyResourceActor())
