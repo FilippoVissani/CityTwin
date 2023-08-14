a87b38774fce38410cc024e8a8f026defe3fb799
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-14 12:14:51 +0200

    feat: remove unused class

--- river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/model/Boundary.scala
+++
@@ -1,8 +0,0 @@
-package it.unibo.citytwin.rivermonitor.model
-
-case class Boundary(x0: Int,
-                    y0: Int,
-                    x1: Int,
-                    y1: Int):
-  def width: Int = x1 - x0
-  def height: Int = y1 - y0
