92471ae400a3af70bc3d4705c1bbacada7e3f4e9
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-14 15:27:59 +0200

    feat: Main adapted to use standardized actors

--- river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/app/Main.scala
+++ river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/app/Main.scala
@@ -4,9 +4,9 @@ import akka.actor.typed.{ActorSystem, Behavior}
 import com.typesafe.config.{Config, ConfigFactory}
 import it.unibo.citytwin.core.actors.MainstayActor
 import it.unibo.citytwin.core.model.Point2D
-import it.unibo.citytwin.rivermonitor.actors.floodsensor.FloodSensorGuardianActor
-import it.unibo.citytwin.rivermonitor.actors.rivermonitor.RiverMonitorGuardianActor
-import it.unibo.citytwin.rivermonitor.actors.view.ViewGuardianActor
+import it.unibo.citytwin.rivermonitor.actors.floodsensor.FloodSensorActor
+import it.unibo.citytwin.rivermonitor.actors.rivermonitor.RiverMonitorActor
+import it.unibo.citytwin.rivermonitor.actors.view.ViewActor
 import it.unibo.citytwin.rivermonitor.model.{FloodSensor, RiverMonitor}
 
 object Main:
@@ -17,18 +17,18 @@ object Main:
     val viewName = "view1"
 
     val floodSensor = FloodSensor(floodSensorName, Point2D[Int](0, 0))
-    startup(port = 2551)(FloodSensorGuardianActor(floodSensor))
+    startup(port = 2551)(FloodSensorActor(floodSensor))
 
     val rmResourcesToCheck = Set[String](floodSensorName, viewName)
     val riverMonitor = RiverMonitor(riverMonitorName, Point2D[Int](0, 0))
-    startup(port = 2552)(RiverMonitorGuardianActor(riverMonitor, rmResourcesToCheck))
+    startup(port = 2552)(RiverMonitorActor(riverMonitor, rmResourcesToCheck))
 
     startup(port = 2553)(MainstayActor())
 
     val width = 600
     val height = 200
     val vResourcesToCheck = Set[String](riverMonitorName)
-    startup(port = 2554)(ViewGuardianActor(viewName, vResourcesToCheck, width, height))
+    startup(port = 2554)(ViewActor(viewName, vResourcesToCheck, width, height))
 
   private def startup[X](file: String = "cluster", port: Int)(root: => Behavior[X]): ActorSystem[X] =
     // Override the configuration of the port
