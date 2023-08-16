8d55847d114b9b755ac6d50fba05d3be4d267e85
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-16 15:50:06 +0200

    fix: adapt system starter to new cluster config

--- core/src/main/scala/it/unibo/citytwin/core/ActorSystemStarter.scala
+++ core/src/main/scala/it/unibo/citytwin/core/ActorSystemStarter.scala
@@ -4,8 +4,8 @@ import akka.actor.typed.{ActorSystem, Behavior}
 import com.typesafe.config.{Config, ConfigFactory}
 
 object ActorSystemStarter:
-  def startup[X](file: String = "cluster")(root: => Behavior[X]): ActorSystem[X] =
-    val config: Config = ConfigFactory.load(file)
+  def startup[X](root: => Behavior[X]): ActorSystem[X] =
+    val config: Config = ConfigFactory.load()
     val clusterName = config.getString("clustering.cluster.name")
     // Create an Akka system
     ActorSystem(root, clusterName)
