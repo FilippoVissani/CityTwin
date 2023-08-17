cd47912ac0469bc9bab961693ae67c6f341d319b
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-17 17:09:40 +0200

    chore: now startup script starts a new window for each node

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,16 +6,10 @@ unzip ./core/target/universal/core-0.1.0.zip -d ./core/target/
 unzip ./control-panel/target/universal/control-panel-0.1.0.zip -d ./control-panel/target/
 unzip ./river-monitor/target/universal/river-monitor-0.1.0.zip -d ./river-monitor/target/
 
-./core/target/core-0.1.0/bin/core 2551 &
-
-./core/target/core-0.1.0/bin/core 2552 &
-
-./core/target/core-0.1.0/bin/core 2553 &
-
-./control-panel/target/control-panel-0.1.0/bin/control-panel 2554 &
-
-./river-monitor/target/river-monitor-0.1.0/bin/river-monitor-main 2555 &
-
-./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main 2556 &
-
-./river-monitor/target/river-monitor-0.1.0/bin/view-main 2557 &
+$TERM -e ./core/target/core-0.1.0/bin/core 2551 &
+$TERM -e ./core/target/core-0.1.0/bin/core 2552 &
+$TERM -e ./core/target/core-0.1.0/bin/core 2553 &
+$TERM -e ./control-panel/target/control-panel-0.1.0/bin/control-panel 2554 &
+$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/river-monitor-main 2555 &
+$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main 2556 &
+$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/view-main 2557 &
