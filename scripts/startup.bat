c24b3768bd68cf1f881d5b24948c32787ce22d5d
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-17 17:40:08 +0200

    chore: startup script for windows

---
+++ scripts/startup.bat
@@ -0,0 +1,15 @@
+call sbt clean
+
+call sbt universal:packageBin
+
+tar -x -f ./core/target/universal/core-0.1.0.zip -C ./core/target/
+tar -x -f ./control-panel/target/universal/control-panel-0.1.0.zip -C ./control-panel/target/
+tar -x -f ./river-monitor/target/universal/river-monitor-0.1.0.zip -C ./river-monitor/target/
+
+start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2551
+start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2552
+start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2553
+start cmd /k call ./control-panel/target/control-panel-0.1.0/bin/control-panel.bat 2554 16000 9000
+start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/river-monitor-main.bat 2555
+start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main.bat 2556
+start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/view-main.bat 2557
