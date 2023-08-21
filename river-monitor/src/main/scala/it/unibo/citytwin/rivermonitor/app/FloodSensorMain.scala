7bd149503b7b6282667f70d2f13ecdaa76d9ed60
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-21 11:56:09 +0200

    feat: floodSensorMain takes all parameters as argument

--- river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/app/FloodSensorMain.scala
+++ river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/app/FloodSensorMain.scala
@@ -6,10 +6,12 @@ import it.unibo.citytwin.rivermonitor.actors.floodsensor.FloodSensorActor
 import it.unibo.citytwin.rivermonitor.model.FloodSensor
 
 @main def floodSensorMain(args: String*): Unit =
-  val floodSensorName  = "floodSensor1"
-  val riverMonitorName = "riverMonitor1"
-  val viewName         = "view1"
-
-  val floodSensor = FloodSensor(floodSensorName, Point2D[Int](4800, 4500))
-  val port: Int   = if args.isEmpty then 2551 else args(0).toInt
-  startup(port)(FloodSensorActor(floodSensor))
+  if args.length < 4 then
+    println("Usage: floodSensorMain <Port> <floodSensorName> <x_position> <y_position>")
+  else
+    val port = args(0).toInt
+    val floodSensorName = args(1)
+    val positionX = args(2).toInt
+    val positionY = args(3).toInt
+    val floodSensor = FloodSensor(floodSensorName, Point2D[Int](positionX, positionY))
+    startup(port)(FloodSensorActor(floodSensor))
