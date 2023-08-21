61493c69b235dc17ae2e575c2b07b09f18b4e794
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-21 14:29:06 +0200

    feat: viewMain takes all parameters as argument

--- river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/app/ViewMain.scala
+++ river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/app/ViewMain.scala
@@ -4,11 +4,14 @@ import it.unibo.citytwin.core.ActorSystemStarter.startup
 import it.unibo.citytwin.rivermonitor.actors.view.ViewActor
 
 @main def viewMain(args: String*): Unit =
-  val riverMonitorName = "riverMonitor1"
-  val viewName         = "view1"
+  if args.length < 5 then
+    println("Usage: viewMain <Port> <viewName> <width> <height> [<resources_to_check>...]")
+  else {
+    val port = args(0).toInt
+    val viewName = args(1)
+    val width = args(2).toInt
+    val height = args(3).toInt
+    val resourcesToCheck = args.drop(4).toSet
+    startup(port)(ViewActor(viewName, resourcesToCheck, width, height))
+  }
 
-  val width             = 600
-  val height            = 200
-  val vResourcesToCheck = Set[String](riverMonitorName)
-  val port: Int         = if args.isEmpty then 2551 else args(0).toInt
-  startup(port)(ViewActor(viewName, vResourcesToCheck, width, height))
--- scripts/startup.bat
+++ scripts/startup.bat
@@ -12,4 +12,4 @@ start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2553
 start cmd /k call ./control-panel/target/control-panel-0.1.0/bin/control-panel.bat 2554 16000 9000
 start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/river-monitor-main.bat 2555 riverMonitor1 100 100 floodSensor1 view1
 start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main.bat 2556 floodSensor1 4800 4500
-start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/view-main.bat 2557
+start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/view-main.bat 2557 view1 600 200 riverMonitor1
