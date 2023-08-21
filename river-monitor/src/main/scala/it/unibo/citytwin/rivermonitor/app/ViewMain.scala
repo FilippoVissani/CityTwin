866cafcb233dd28e4542ccde9f839af68b48b821
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-21 15:46:54 +0200

    chore: specify in the viewMain that check for only one riverMonitor

--- river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/app/ViewMain.scala
+++ river-monitor/src/main/scala/it/unibo/citytwin/rivermonitor/app/ViewMain.scala
@@ -4,14 +4,14 @@ import it.unibo.citytwin.core.ActorSystemStarter.startup
 import it.unibo.citytwin.rivermonitor.actors.view.ViewActor
 
 @main def viewMain(args: String*): Unit =
-  if args.length < 5 then
-    println("Usage: viewMain <Port> <viewName> <width> <height> [<resources_to_check>...]")
+  if args.length != 5 then
+    println("Usage: viewMain <Port> <viewName> <width> <height> <riverMonitor_to_check>")
   else {
     val port = args(0).toInt
     val viewName = args(1)
     val width = args(2).toInt
     val height = args(3).toInt
-    val resourcesToCheck = args.drop(4).toSet
-    startup(port)(ViewActor(viewName, resourcesToCheck, width, height))
+    val riverMonitorToCheck = args.drop(4).toSet
+    startup(port)(ViewActor(viewName, riverMonitorToCheck, width, height))
   }
 
