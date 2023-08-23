ac7dc2fc964e6fa9aaeead536973dd2a493167f9
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-23 10:34:11 +0200

    fix: add parameters in bat script

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -6,9 +6,9 @@ tar -x -f ./core/target/universal/core-0.1.0.zip -C ./core/target/
 tar -x -f ./control-panel/target/universal/control-panel-0.1.0.zip -C ./control-panel/target/
 tar -x -f ./river-monitor/target/universal/river-monitor-0.1.0.zip -C ./river-monitor/target/
 
-start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2551
-start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2552
-start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2553
+start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2551 127.0.0.1 8080
+start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2552 127.0.0.1 8080
+start cmd /k call ./core/target/core-0.1.0/bin/core.bat 2553 127.0.0.1 8080
 start cmd /k call ./control-panel/target/control-panel-0.1.0/bin/control-panel.bat 2554 16000 9000
 start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/river-monitor-main.bat 2555 riverMonitor1 15 100 100 floodSensor1 view1 floodSensor2
 start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main.bat 2556 floodSensor1 4800 4500
