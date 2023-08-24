57d6cb87eaab34a5309fc1adec7552b339ebf7fb
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-24 12:15:21 +0200

    chore: change resources names and positions in startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -12,9 +12,9 @@ $TERM -e ./core/target/core-0.1.0/bin/core 2551 127.0.0.1 8080 &
 $TERM -e ./core/target/core-0.1.0/bin/core 2552 127.0.0.1 8080 &
 $TERM -e ./core/target/core-0.1.0/bin/core 2553 127.0.0.1 8080 &
 $TERM -e ./control-panel/target/control-panel-0.1.0/bin/control-panel 2554 16000 9000 &
-$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/river-monitor-main 2555 riverMonitor1 15 100 100 floodSensor1 view1 floodSensor2 &
-$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main 2556 floodSensor1 4800 4500 &
-$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main 2558 floodSensor2 4500 6500 &
-$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/view-main 2557 view1 600 200 riverMonitor1 &
-$TERM -e ./air-quality-monitor/target/air-quality-monitor-0.1.0/bin/air-quality-monitor.bat 2559 AirSensor_ParcoDelleQuerce 6500 6500 "https://air-quality-sensor-cesena.azurewebsites.net/api/getAirValues?code=o_cTLAmHm8d4UvdWC89wVKRqfUMGMF55x0sA9FDGTsczAzFuTYg6bg=="
+$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/river-monitor-main 2555 RiverMonitor_Savio 15 5200 5600 FloodSensor_PonteVecchio FloodSensor_PonteNuovo View_Savio &
+$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main 2556 FloodSensor_PonteVecchio 4500 7100 &
+$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main 2557 FloodSensor_PonteNuovo 4900 4200 &
+$TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/view-main 2558 View_Savio 600 200 RiverMonitor_Savio &
+$TERM -e ./air-quality-monitor/target/air-quality-monitor-0.1.0/bin/air-quality-monitor.bat 2559 AirSensor_ParcoDelleQuerce 6800 6600 "https://air-quality-sensor-cesena.azurewebsites.net/api/getAirValues?code=o_cTLAmHm8d4UvdWC89wVKRqfUMGMF55x0sA9FDGTsczAzFuTYg6bg=="
 $TERM -e ./acid-rain-monitor/target/acid-rain-monitor-0.1.0/bin/acid-rain-monitor.bat 2560 AcidRainSensor_Ippodromo 3000 500
