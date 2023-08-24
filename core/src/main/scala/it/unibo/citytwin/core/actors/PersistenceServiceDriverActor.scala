60bd8a8836554348c64f7d220ea7eef640d7b00d
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-24 19:08:38 +0200

    feat: now PersistenceServiceDriverActor can request mainstays and resources history

--- core/src/main/scala/it/unibo/citytwin/core/actors/PersistenceServiceDriverActor.scala
+++ core/src/main/scala/it/unibo/citytwin/core/actors/PersistenceServiceDriverActor.scala
@@ -1,24 +1,38 @@
 package it.unibo.citytwin.core.actors
 
-import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, scaladsl}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.client.RequestBuilding.Post
+import akka.http.scaladsl.model.HttpResponse.unapply
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethod, HttpMethods, HttpRequest, HttpResponse}
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
+import it.unibo.citytwin.core.JSONParser.{jsonToMainstaysHistory, jsonToResourcesHistory}
 import it.unibo.citytwin.core.model.Resource
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
+
+import java.time.{LocalDate, LocalDateTime}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
+import concurrent.duration.DurationInt
 
 trait PersistenceServiceDriverActorCommand
 
+case class AskMainstaysHistory(replyTo: ActorRef[MainstaysHistoryResponse]) extends PersistenceServiceDriverActorCommand
+  with Serializable
+
+case class AskResourcesHistory(replyTo: ActorRef[ResourcesHistoryResponse]) extends PersistenceServiceDriverActorCommand
+  with Serializable
 case class PostMainstay(address: String, state: Boolean) extends PersistenceServiceDriverActorCommand
   with Serializable
 
 case class PostResource(address: String, resource: Resource) extends PersistenceServiceDriverActorCommand
   with Serializable
+
+case class MainstaysHistoryResponse(states: Seq[(Boolean, LocalDateTime)])
+
+case class ResourcesHistoryResponse(states: Seq[(Boolean, LocalDateTime)])
 
 object PersistenceServiceDriverActor:
 
@@ -28,11 +42,36 @@ object PersistenceServiceDriverActor:
       implicit val executionContext: ExecutionContext = ctx.executionContext
       implicit val system: ActorSystem[Nothing] = ctx.system
       Behaviors.receiveMessage {
+        case AskMainstaysHistory(replyTo: ActorRef[MainstaysHistoryResponse]) => {
+          ctx.log.debug("Received AskMainstayHistory")
+          val response: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = s"http://$host:$port/mainstay?address=*"))
+          response
+            .flatMap(resp => resp.entity.toStrict(1.seconds))
+            .map(strictEntity => strictEntity.data.utf8String)
+            .onComplete {
+              case Success(result) => replyTo ! MainstaysHistoryResponse(jsonToMainstaysHistory(result))
+              case Failure(exception) => println(exception)
+            }
+          Behaviors.same
+        }
+        case AskResourcesHistory(replyTo: ActorRef[ResourcesHistoryResponse]) => {
+          ctx.log.debug("Received AskResourcesHistory")
+          val response: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = s"http://$host:$port/resource?name=*"))
+          response
+            .flatMap(resp => resp.entity.toStrict(1.seconds))
+            .map(strictEntity => strictEntity.data.utf8String)
+            .onComplete {
+              case Success(result) => replyTo ! ResourcesHistoryResponse(jsonToResourcesHistory(result))
+              case Failure(exception) => println(exception)
+            }
+          Behaviors.same
+        }
         case PostMainstay(address: String, state: Boolean) => {
           ctx.log.debug("Received PostMainstay")
           val body = Json.obj(
             "address" -> address,
             "state" -> state,
+            "time" -> LocalDateTime.now(),
           ).toString()
           Http().singleRequest(HttpRequest(
             method = HttpMethods.POST,
@@ -48,6 +87,7 @@ object PersistenceServiceDriverActor:
             "name" -> resource.name.orNull,
             "state" -> resource.state.orNull,
             "resource_type" -> resource.resourceType,
+            "time" -> LocalDateTime.now(),
           )
           if resource.position.isDefined then
             body = body ++ Json.obj(
