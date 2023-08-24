80238a0ed98ca2b92a275dc029eb0ac34d0e62c9
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-24 19:06:31 +0200

    feat: add object to parse json responses

---
+++ core/src/main/scala/it/unibo/citytwin/core/JSONParser.scala
@@ -0,0 +1,22 @@
+package it.unibo.citytwin.core
+
+import it.unibo.citytwin.core.model.Resource
+import play.api.libs.json.{JsValue, Json}
+
+import java.time.LocalDateTime
+
+object JSONParser:
+
+  def jsonToMainstaysHistory(jsonString: String): Seq[(Boolean, LocalDateTime)] =
+    val json: JsValue = Json.parse(jsonString)
+    val statesHistory = (json \\ "state").map(v => v.asOpt[Boolean])
+    val datesHistory = (json \\ "time").map(v => v.asOpt[String].map(x => LocalDateTime.parse(x)))
+    val mergedHistory: Seq[(Boolean, LocalDateTime)] = statesHistory.zip(datesHistory).filter((s, t) => s.isDefined && t.isDefined).map((s, t) => (s.get, t.get)).toSeq
+    mergedHistory
+
+  def jsonToResourcesHistory(jsonString: String): Seq[(Boolean, LocalDateTime)] =
+    val json: JsValue = Json.parse(jsonString)
+    val statesHistory = (json \\ "node_state").map(v => v.asOpt[Boolean])
+    val datesHistory = (json \\ "time").map(v => v.asOpt[String].map(x => LocalDateTime.parse(x)))
+    val mergedHistory: Seq[(Boolean, LocalDateTime)] = statesHistory.zip(datesHistory).filter((s, t) => s.isDefined && t.isDefined).map((s, t) => (s.get, t.get)).toSeq
+    mergedHistory
