bcb8ec0ca58e2c5c13e8c106f819dc3ddbc62550
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-25 11:25:35 +0200

    fix: startup bash script was callin batch files

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -17,6 +17,6 @@ $TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/river-monitor-main 2555 
 $TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main 2556 FloodSensor_PonteVecchio 4500 7100 &
 $TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main 2557 FloodSensor_PonteNuovo 4900 4200 &
 $TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/view-main 2558 View_Savio 600 200 RiverMonitor_Savio &
-$TERM -e ./air-quality-monitor/target/air-quality-monitor-0.1.0/bin/air-quality-monitor.bat 2559 AirSensor_ParcoDelleQuerce 6800 6600 "https://air-quality-sensor-cesena.azurewebsites.net/api/getAirValues?code=o_cTLAmHm8d4UvdWC89wVKRqfUMGMF55x0sA9FDGTsczAzFuTYg6bg==" &
-$TERM -e ./acid-rain-monitor/target/acid-rain-monitor-0.1.0/bin/acid-rain-monitor.bat 2560 AcidRainSensor_Ippodromo 3000 500 &
-$TERM -e ./noise-pollution-monitor/target/noise-pollution-monitor-0.1.0/bin/noise-pollution-monitor.bat 2561 NoiseSensor_Scuola 2000 5000 &
+$TERM -e ./air-quality-monitor/target/air-quality-monitor-0.1.0/bin/air-quality-monitor 2559 AirSensor_ParcoDelleQuerce 6800 6600 "https://air-quality-sensor-cesena.azurewebsites.net/api/getAirValues?code=o_cTLAmHm8d4UvdWC89wVKRqfUMGMF55x0sA9FDGTsczAzFuTYg6bg==" &
+$TERM -e ./acid-rain-monitor/target/acid-rain-monitor-0.1.0/bin/acid-rain-monitor 2560 AcidRainSensor_Ippodromo 3000 500 &
+$TERM -e ./noise-pollution-monitor/target/noise-pollution-monitor-0.1.0/bin/noise-pollution-monitor 2561 NoiseSensor_Scuola 2000 5000 &
