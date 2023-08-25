3323bdce2af1ee186d36d4708cb16916e92b7d72
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-25 11:32:15 +0200

    test: change JSON test name

--- core/src/test/scala/it/unibo/citytwin/core/JsonParseTest.scala
+++ core/src/test/scala/it/unibo/citytwin/core/JsonParseTest.scala
@@ -11,7 +11,7 @@ import java.time.{LocalDate, LocalDateTime}
 class JsonParseTest extends AnyWordSpec with Matchers:
 
   "Parser" should {
-    "Parse correctly a JSON string" in {
+    "Parse correctly mainstays history JSON string" in {
       val jsonString =
         "[\n    {\n        \"_id\": \"64e780ad30d4ded1359bf52e\",\n        \"address\": \"akka://ClusterSystem@127.0.0.1:2551/user\",\n        \"state\": true,\n        \"time\": \"2023-08-24T18:09:17.581426074\"\n    },\n    {\n        \"_id\": \"64e780ae30d4ded1359bf534\",\n        \"address\": \"akka://ClusterSystem@127.0.0.1:2551/user\",\n        \"state\": true,\n        \"time\": \"2023-08-24T18:09:17.996343216\"\n    },\n    {\n        \"_id\": \"64e780b030d4ded1359bf546\",\n        \"address\": \"akka://ClusterSystem@127.0.0.1:2551/user\",\n        \"state\": true,\n        \"time\": \"2023-08-24T18:09:20.073069294\"\n    },\n    {\n        \"_id\": \"64e780b130d4ded1359bf555\",\n        \"address\": \"akka://ClusterSystem@127.0.0.1:2551/user\",\n        \"state\": true,\n        \"time\": \"2023-08-24T18:09:21.739939386\"\n    }\n]"
       JSONParser.jsonToMainstaysHistory(jsonString).map((x, y) => (x, y.toString)) shouldBe List((MainstayState(Some("akka://ClusterSystem@127.0.0.1:2551/user"), Some(true)), "2023-08-24T18:09:17.581426074"), (MainstayState(Some("akka://ClusterSystem@127.0.0.1:2551/user"), Some(true)), "2023-08-24T18:09:17.996343216"), (MainstayState(Some("akka://ClusterSystem@127.0.0.1:2551/user"), Some(true)), "2023-08-24T18:09:20.073069294"), (MainstayState(Some("akka://ClusterSystem@127.0.0.1:2551/user"), Some(true)), "2023-08-24T18:09:21.739939386"))
