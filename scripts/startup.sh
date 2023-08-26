8c778115fb26f7dae98267ed222ae3e3022ce010
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-26 11:57:07 +0200

    chore: add new sensor in startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -19,4 +19,6 @@ $TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-main 2557 F
 $TERM -e ./river-monitor/target/river-monitor-0.1.0/bin/view-main 2558 View_Savio 600 200 RiverMonitor_Savio &
 $TERM -e ./air-quality-monitor/target/air-quality-monitor-0.1.0/bin/air-quality-monitor 2559 AirSensor_ParcoDelleQuerce 6800 6600 "https://air-quality-sensor-cesena.azurewebsites.net/api/getAirValues?code=o_cTLAmHm8d4UvdWC89wVKRqfUMGMF55x0sA9FDGTsczAzFuTYg6bg==" &
 $TERM -e ./acid-rain-monitor/target/acid-rain-monitor-0.1.0/bin/acid-rain-monitor 2560 AcidRainSensor_Ippodromo 3000 500 &
-$TERM -e ./noise-pollution-monitor/target/noise-pollution-monitor-0.1.0/bin/noise-pollution-monitor 2561 NoiseSensor_Scuola 2000 5000 &
+$TERM -e ./acid-rain-monitor/target/acid-rain-monitor-0.1.0/bin/acid-rain-monitor 2561 AcidRainSensor_GiardinoPubblico 11000 6500 &
+$TERM -e ./noise-pollution-monitor/target/noise-pollution-monitor-0.1.0/bin/noise-pollution-monitor 2562 NoiseSensor_Scuola 2000 5000 &
+$TERM -e ./noise-pollution-monitor/target/noise-pollution-monitor-0.1.0/bin/noise-pollution-monitor 2563 NoiseSensor_Bufalini 14000 7000 &
