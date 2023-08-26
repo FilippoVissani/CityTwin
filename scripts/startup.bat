8e680876b9499c1aaabdd1fea0433df5c28ba39e
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-08-26 15:01:49 +0200

    fix: start a sensor with correct parameters

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -19,6 +19,6 @@ start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/flood-sensor-ma
 start cmd /k call ./river-monitor/target/river-monitor-0.1.0/bin/view-main.bat 2558 View_Savio 600 200 RiverMonitor_Savio
 start cmd /k call ./air-quality-monitor/target/air-quality-monitor-0.1.0/bin/air-quality-monitor.bat 2559 AirSensor_ParcoDelleQuerce 6800 6600 "https://air-quality-sensor-cesena.azurewebsites.net/api/getAirValues?code=o_cTLAmHm8d4UvdWC89wVKRqfUMGMF55x0sA9FDGTsczAzFuTYg6bg=="
 start cmd /k call ./acid-rain-monitor/target/acid-rain-monitor-0.1.0/bin/acid-rain-monitor.bat 2560 AcidRainSensor_Ippodromo 3000 500
-start cmd /k call ./acid-rain-monitor/target/acid-rain-monitor-0.1.0/bin/acid-rain-monitor.bat 2561 AcidRainSensor_GiardinoPubblico 11000 6500 3000 500
+start cmd /k call ./acid-rain-monitor/target/acid-rain-monitor-0.1.0/bin/acid-rain-monitor.bat 2561 AcidRainSensor_GiardinoPubblico 11000 6500
 start cmd /k call ./noise-pollution-monitor/target/noise-pollution-monitor-0.1.0/bin/noise-pollution-monitor.bat 2562 NoiseSensor_Scuola 2000 5000
 start cmd /k call ./noise-pollution-monitor/target/noise-pollution-monitor-0.1.0/bin/noise-pollution-monitor.bat 2563 NoiseSensor_Bufalini 14000 7000
