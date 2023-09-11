bf9214717bf59896b2f75590ed5ad976ae5b8c51
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-09-11 11:28:51 +0200

    test: adapt test to new feature

--- core/src/test/scala/it/unibo/citytwin/core/model/ResourceStateTest.scala
+++ core/src/test/scala/it/unibo/citytwin/core/model/ResourceStateTest.scala
@@ -5,18 +5,31 @@ import it.unibo.citytwin.core.model.{Point2D, ResourceState}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
+import java.time.LocalDateTime
+
 class ResourceStateTest extends AnyWordSpec with Matchers:
   "Resource" should {
     "Merge correctly another resource" in {
       val resource1 =
-        ResourceState(name = Some("sensor1"), nodeState = Some(true), position = Some(Point2D(0, 0)))
+        ResourceState(
+          name = Some("sensor1"),
+          nodeState = Some(true),
+          position = Some(Point2D(0, 0)),
+          time = Some(LocalDateTime.MAX),
+        )
       val resource2 =
-        ResourceState(name = Some("sensor1"), resourceType = Set(Act), position = Some(Point2D(1, 1)))
+        ResourceState(
+          name = Some("sensor1"),
+          resourceType = Set(Act),
+          position = Some(Point2D(1, 1)),
+          time = Some(LocalDateTime.MIN)
+        )
       val result = ResourceState(
         name = Some("sensor1"),
         resourceType = Set(Act),
         nodeState = Some(true),
-        position = Some(Point2D(1, 1))
+        position = Some(Point2D(0, 0)),
+        time = Some(LocalDateTime.MAX),
       )
       resource1.merge(resource2) shouldBe result
     }
